9d706e058a6af70ca7f891a8a67118539f8c7963
CREATODIDAK <96537473+Creatodidak@users.noreply.github.com>
2023-08-27 01:01:49 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+function extractStrings($input) {
+    preg_match_all("/'([^']+)'/", $input, $matches);
+    $result = implode("', '", $matches[1]);
+    return $result;
+}
+
+$inputText = "";
+$outputText = "";
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $inputText = $_POST["inputTextArea"];
+    $outputText = extractStrings($inputText);
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>String Extraction</title>
+</head>
+<body>
+    <h2>String Extraction</h2>
+    <form method="post">
+        <label for="inputTextArea">Input Text:</label><br>
+        <textarea name="inputTextArea" rows="5" cols="40"><?php echo $inputText; ?></textarea><br>
+        <button type="submit">Extract Strings</button>
+    </form>
+    <br>
+    <label for="outputTextArea">Output Text:</label><br>
+    <textarea name="outputTextArea" rows="5" cols="40"><?php echo $outputText; ?></textarea>
+</body>
+</html>
